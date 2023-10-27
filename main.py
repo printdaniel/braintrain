1b1878645ce0a9dd921df55adefdddfed36abef7
Daniel <dwsuarez98@gmail.com>
2023-10-27 15:49:39 -0300

    fix: errores al continuar el juego

--- main.py
+++ main.py
@@ -25,22 +25,12 @@ class BrainTrain:
         self.n6 = StringVar()
         self.n7 = StringVar()
         self.n8 = StringVar()
-        self.n9 = StringVar()
-        self.n10 = StringVar()
-        self.n11 = StringVar()
-        self.n12 = StringVar()
-        self.n13 = StringVar()
-        self.n14 = StringVar()
-        self.n15 = StringVar()
-        self.n16 = StringVar()
-
-        self.numeros = [self.n1, self.n2, self.n3, self.n4, self.n5, self.n6, self.n7, self.n8,
-                   self.n9, self.n10, self.n11, self.n12, self.n13, self.n14, self.n15, self.n16]
+        
+        self.numeros = [self.n1, self.n2, self.n3, self.n4, self.n5, self.n6, 
+                        self.n7, self.n8]
 
         self.r1 = StringVar()
         self.r2 = StringVar()
-        self.r3 = StringVar()
-        self.r4 = StringVar()
         
         self.frame1 = LabelFrame(self.root,text="Buena suerte!!!")
         self.frame1.grid(row=3, column=0)
@@ -49,6 +39,7 @@ class BrainTrain:
         self.frame2.grid(row=4, column=0)
 
       
+        # Labels para los 8 números a sumar
         self.num1 = Label(self.frame,font=('arial',15),textvariable=self.n1)
         self.num2 = Label(self.frame,font=('arial',15),textvariable=self.n2)
         self.num3 = Label(self.frame,font=('arial',15),textvariable=self.n3)
@@ -56,26 +47,10 @@ class BrainTrain:
         self.num5 = Label(self.frame,font=('arial',15),textvariable=self.n5)
         self.num6 = Label(self.frame,font=('arial',15),textvariable=self.n6)
         self.num7 = Label(self.frame,font=('arial',15),textvariable=self.n7)
-        self.num8 = Label(self.frame,font=('arial',15),textvariable=self.n8)
-        self.num9 = Label(self.frame,font=('arial',15),textvariable=self.n9)
-        self.num10 = Label(self.frame,font=('arial',15),textvariable=self.n10)
-        self.num11 = Label(self.frame,font=('arial',15),textvariable=self.n11)
-        self.num12 = Label(self.frame,font=('arial',15),textvariable=self.n12)
-        self.num13 = Label(self.frame,font=('arial',15),textvariable=self.n13)
-        self.num14 = Label(self.frame,font=('arial',15),textvariable=self.n14)
-        self.num15 = Label(self.frame,font=('arial',15),textvariable=self.n15)
-        self.num16 = Label(self.frame,font=('arial',15),textvariable=self.n16)
+        self.num8 = Label(self.frame,font=('arial',15),textvariable=self.n7)
+        
 
-
-        self.alert1 = Label(self.frame, text="   ",bg="red")
-        self.alert1.grid(row=2,column=5)
-        self.alert2 = Label(self.frame, text="   ",bg="red")
-        self.alert2.grid(row=3,column=5)
-        self.alert3 = Label(self.frame, text="   ",bg="red")
-        self.alert3.grid(row=4,column=5)
-        self.alert4 = Label(self.frame, text="   ",bg="red")
-        self.alert4.grid(row=5,column=5)
-
+                
         # Grid
         self.num1.grid(row=2,column=0)
         self.num2.grid(row=2,column=1)
@@ -87,16 +62,7 @@ class BrainTrain:
         self.num7.grid(row=3,column=2)
         self.num8.grid(row=3,column=3)
         
-        self.num9.grid(row=4,column=0)
-        self.num10.grid(row=4,column=1)
-        self.num11.grid(row=4,column=2)
-        self.num12.grid(row=4,column=3)
-
-        self.num13.grid(row=5,column=0)
-        self.num14.grid(row=5,column=1)
-        self.num15.grid(row=5,column=2)
-        self.num16.grid(row=5,column=3)
-
+        
         # Responses
         self.respuesta1 = Entry(self.frame, width="5", font=('arial', 15), textvariable=self.r1)
         self.respuesta1.grid(row=2,column=4)
@@ -104,13 +70,7 @@ class BrainTrain:
         self.respuesta2 = Entry(self.frame, width="5", font=('arial', 15), textvariable=self.r2)
         self.respuesta2.grid(row=3,column=4)
 
-        self.respuesta3 = Entry(self.frame, width="5", font=('arial', 15), textvariable=self.r3)
-        self.respuesta3.grid(row=4,column=4)
-
-        self.respuesta4 = Entry(self.frame, width="5", font=('arial', 15), textvariable=self.r4)
-        self.respuesta4.grid(row=5,column=4)
         
-
         com_buton=Button(self.frame1,text="¡Voy a sumar!",command=self.num_random,
                          font=('arial',20,BOLD),relief=GROOVE)
         com_buton.grid(row=0,column=0)
@@ -127,6 +87,14 @@ class BrainTrain:
         self.treeview.heading('tiempo', text='Tiempo')
         self.treeview.heading('fecha', text='Fecha')
         self.mostrar_records()
+
+        self.red_alerts()
+
+    def red_alerts(self):
+        self.alert1 = Label(self.frame, text="   ",bg="red")
+        self.alert1.grid(row=2,column=5)
+        self.alert2 = Label(self.frame, text="   ",bg="red")
+        self.alert2.grid(row=3,column=5)
 
     def mostrar_records(self):
         """ 
@@ -163,6 +131,10 @@ class BrainTrain:
         Returns:
             None
         """
+
+        # Resetea las alertas en rojo
+        self.red_alerts()
+
         # Iniciar tiempo de juego
         self.start_time = time.time()
 
@@ -223,20 +195,7 @@ class BrainTrain:
 
         suma_2 = sum(map(int,suma_fila_2))
 
-        # Sumas fila tres
-        n9, n10, n11, n12 = [var.get() for var in (self.n9, self.n10, self.n11, self.n12)]
-        suma_fila_3 = [n9, n10,n11,n12]
-
-        suma_3 = sum(map(int,suma_fila_3))
-
-        n13, n14, n15, n16 = [var.get() for var in (self.n13, self.n14, self.n15, self.n16)]
-        suma_fila_4 = [n13, n14,n15,n16]
-
-        # Sumas fila 4
-        suma_4 = sum(map(int,suma_fila_4))
-
-        return suma_1, suma_2, suma_3, suma_4
-
+        return suma_1, suma_2
 
     def verificar_sumas(self):
         """ 
@@ -245,17 +204,15 @@ class BrainTrain:
         Returns:
         None
         """
-        # Obtener las sumas de cada fila
-        suma_1, suma_2, suma_3, suma_4 = self.obtener_sumas()
+                # Obtener las sumas de cada fila
+        suma_1, suma_2 = self.obtener_sumas()
+        print(suma_1, suma_2)
 
         # Obtener cada una de las respuestas del usuario.
         intento_f1 = self.r1.get()
         intento_f2 = self.r2.get()
-        intento_f3 = self.r3.get()
-        intento_f4 = self.r4.get()
 
-
-        # Bantera, para saber si se logró dar con las 4 sumas.
+        # Bantera, para saber si se logró dar con las 2 sumas.
         condicones = 0
 
         try:
@@ -269,20 +226,11 @@ class BrainTrain:
                 self.alert2 = Label(self.frame, text="   ",bg="green")
                 self.alert2.grid(row=3,column=5)
             
-            if suma_3 == int(intento_f3):
-                condicones += 1
-                self.alert3 = Label(self.frame, text="   ",bg="green")
-                self.alert3.grid(row=4,column=5)
             
-            if suma_4 == int(intento_f4):
-                condicones += 1
-                self.alert4 = Label(self.frame, text="   ",bg="green")
-                self.alert4.grid(row=5,column=5)
-
         except:
             print("Los campos estan vacíos")
 
-        if condicones == 4:
+        if condicones == 2:
             if self.start_time is not None:
                 time_enlapsed = time.time() - self.start_time
                 minutes, seconds = divmod(time_enlapsed, 60)
